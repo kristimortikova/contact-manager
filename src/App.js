29e6fc5e4e82ac00ae95d5c8c73b6ba97a63b11d
kristimortikova <kristimortikova@gmail.com>
2018-02-16 22:50:58 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={clicked:0};
+    this.handleClick=this.handleClick.bind(this);
+
+  }
+  handleClick(){
+    this.setState({clicked: this.state.clicked+1});
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome</h1>
+        </header>
+        <p className="App-intro" onClick={this.handleClick}>
+          To get started, edit <code>src/App.js</code> and save to reload.
+          {this.state.clicked}
+        </p>
+        <p className="Google-link">
+        This is <strong>link</strong>
+        &nbsp;to&nbsp;
+        <a href="https://www.google.ua">Google</a>
+
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
