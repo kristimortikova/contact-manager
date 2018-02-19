d58b61f6eaf9df2abc5b4c0611f08b0d42197800
kristimortikova <kristimortikova@gmail.com>
2018-02-19 18:13:55 +0200

    changed styles for contacts

--- src/Contacts.js
+++ src/Contacts.js
@@ -10,31 +10,56 @@ class ContactForm extends Component{
 		this.handleClick=this.handleClick.bind(this);
 	}
 	handleChange(event){
-		console.log(event.target.name, event.target.value);
 		this.setState({
 			[event.target.name]:event.target.value,
 		})
 	}
 	handleClick(event){
 		event.preventDefault();
+		this.props.onAdd(this.state)
 		this.setState({name:'', email:'',});
 	}	
 	render(){
-		console.log(this.state);
-		return(<form onChange = {this.handleChange}>
-    		<div>Name <input name = 'name' value={this.state.name}/></div>
-    		<div>Email<input name = 'email' value={this.state.email}/></div>
+		return(<form onChange={this.handleChange}>
+    		<div>Name <input name='name' value={this.state.name}/></div>
+    		<div>Email<input name='email' value={this.state.email}/></div>
     		<button id="add" onClick={this.handleClick}> add </button>
     	</form> );
 	}
 }
+class Contact extends Component{
+	render(){
+		return (
+    		<div className='Contact'>
+    			<div className='Contact-name'>{this.props.name}</div> 
+    			<div className='Contact-email'>{this.props.email}</div>
+    		</div>);
+	}
+}
 
 export default class Contacts extends Component{
-  render(){
-    return (
-    	<div className = 'App'>
-    	<h2> Contact list</h2>
-    	<ContactForm/>
-    	</div>);
-  }
+	constructor(props){
+		super(props);
+		this.state={
+			contacts:[]
+		};
+		this.handleAdd=this.handleAdd.bind(this);
+	}
+	handleAdd(contact){
+		this.setState({
+			contacts:[...this.state.contacts, contact]
+		});
+		
+	}
+
+  	render(){
+    	return (
+    		<div className='App-contacts'>
+    			<h2> Contact list</h2>
+    			<ContactForm onAdd={this.handleAdd}/>
+    			{this.state.contacts.map(function(contact,key){
+    				return (<Contact key={key} name={contact.name} email={contact.email}/>);
+    			})}
+    		</div>);
+  	}
 }
